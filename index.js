0cad360d4baab402fe96ed51013866c40bb18c4d
Russell Wallace <russell.wallace@gmail.com>
2017-04-01 19:30:09 +0100

    convert

--- index.js
+++ index.js
@@ -2,15 +2,43 @@
 
 function lowerNot(sign, a) {
 	switch (a.op) {
+	case '!':
+		return lowerNot(!sign, a.args[0]);
 	case '&&':
 		var args = a.args.map(function (x) {
 			return lowerNot(sign, x);
 		});
 		return {
 			args: args,
-			op: (sign ? '&&' : '||'),
+			op: sign ? '&&' : '||',
+		};
+	case '<=>':
+		var args = [
+			lowerNot(sign, a.args[0]),
+			lowerNot(true, a.args[1]),
+		];
+		return {
+			args: args,
+			op: '<=>',
+		};
+	case '||':
+		var args = a.args.map(function (x) {
+			return lowerNot(sign, x);
+		});
+		return {
+			args: args,
+			op: sign ? '||' : '&&',
 		};
 	}
+	if (sign) {
+		return a;
+	}
+	return {
+		args: [
+			a,
+		],
+		op: '!',
+	};
 }
 
 // API
