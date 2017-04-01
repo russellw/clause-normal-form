cdf52691c41699b3beb118b1374d440385fd2476
Russell Wallace <russell.wallace@gmail.com>
2017-04-01 19:23:37 +0100

    convert

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+function lowerNot(sign, a) {
+	switch (a.op) {
+	case '&&':
+		var args = a.args.map(function (x) {
+			return lowerNot(sign, x);
+		});
+		return {
+			args: args,
+			op: (sign ? '&&' : '||'),
+		};
+	}
+}
+
 // API
 
 function convert(a) {
