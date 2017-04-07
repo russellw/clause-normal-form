85d288b09e2592acfad63ca338fb904fe8ee16c8
Russell Wallace <russell.wallace@gmail.com>
2017-04-07 21:24:02 +0100

    ops

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ function convert(a) {
 
 function lowerNot(sign, a) {
 	switch (a.op) {
-	case '!':
-		return lowerNot(!sign, a.args[0])
 	case '&':
-		var args = a.args.map(
-			function (x) {
-				return lowerNot(sign, x)
-			})
+		var args = a.args.map(x => lowerNot(sign, x))
 		return {
 			args,
 			op: sign ? '&' : '|',
@@ -25,21 +20,35 @@ function lowerNot(sign, a) {
 			args,
 			op: '<=>',
 		}
+	case '<~>':
+		return lowerNot(!sign, {
+			args: a.args,
+			op: '<=>',
+		})
 	case '|':
-		var args = a.args.map(
-			function (x) {
-				return lowerNot(sign, x)
-			})
+		var args = a.args.map(x => lowerNot(sign, x))
 		return {
 			args,
 			op: sign ? '|' : '&',
 		}
+	case '~':
+		return lowerNot(!sign, a.args[0])
+	case '~&':
+		return lowerNot(!sign, {
+			args: a.args,
+			op: '&',
+		})
+	case '~|':
+		return lowerNot(!sign, {
+			args: a.args,
+			op: '|',
+		})
 	}
 	if (sign)
 		return a
 	return {
 		args: [a],
-		op: '!',
+		op: '~',
 	}
 }
 
