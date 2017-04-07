b182eda880982bfc907c3eea21f8535d06f9db19
Russell Wallace <russell.wallace@gmail.com>
2017-04-07 08:30:56 +0100

    clean

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-'use strict';
-
-function lowerNot(sign, a) {
-	switch (a.op) {
-	case '!':
-		return lowerNot(!sign, a.args[0]);
-	case '&&':
-		var args = a.args.map(function (x) {
-			return lowerNot(sign, x);
-		});
-		return {
-			args: args,
-			op: sign ? '&&' : '||',
-		};
-	case '<=>':
-		var args = [
-			lowerNot(sign, a.args[0]),
-			lowerNot(true, a.args[1]),
-		];
-		return {
-			args: args,
-			op: '<=>',
-		};
-	case '||':
-		var args = a.args.map(function (x) {
-			return lowerNot(sign, x);
-		});
-		return {
-			args: args,
-			op: sign ? '||' : '&&',
-		};
-	}
-	if (sign) {
-		return a;
-	}
-	return {
-		args: [
-			a,
-		],
-		op: '!',
-	};
-}
-
-// Exports
+'use strict'
 
 function convert(a) {
 }
 
-exports.convert = convert;
+function lowerNot(sign, a) {
+	switch (a.op) {
+	case '!':
+		return lowerNot(!sign, a.args[0])
+	case '&&':
+		var args = a.args.map(
+			function (x) {
+				return lowerNot(sign, x)
+			})
+		return {
+			args,
+			op: sign ? '&&' : '||',
+		}
+	case '<=>':
+		var args = [
+			lowerNot(sign, a.args[0]),
+			lowerNot(true, a.args[1]),
+		]
+		return {
+			args,
+			op: '<=>',
+		}
+	case '||':
+		var args = a.args.map(
+			function (x) {
+				return lowerNot(sign, x)
+			})
+		return {
+			args,
+			op: sign ? '||' : '&&',
+		}
+	}
+	if (sign)
+		return a
+	return {
+		args: [a],
+		op: '!',
+	}
+}
+
+exports.convert = convert
