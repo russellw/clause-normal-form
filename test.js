3740836dd1f4b8e5ef7413d259e143e0b5068075
Russell Wallace <russell.wallace@gmail.com>
2017-04-18 15:09:26 +0100

    test

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
 var assert = require('assert')
 var index = require('./index')
 
-describe('convert', () => {
-	it('constant', () => {
-		var a = {
-			args: [],
-			op: 'fun',
-		}
-		var c = {
-			args: [a],
-			op: '|',
-		}
-		var cs = [c]
-		assert.deepEqual(index.convert(a), cs)
-	})
+it('constant', () => {
+	var a = {
+		args: [],
+		op: 'fun',
+	}
+	var c = {
+		args: [a],
+		op: '|',
+	}
+	var cs = [c]
+	assert.deepEqual(index.convert(a), cs)
 })
