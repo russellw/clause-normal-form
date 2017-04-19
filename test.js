2f0bd2756722fc1d9f2755cbe98d48f04e4144ff
Russell Wallace <russell.wallace@gmail.com>
2017-04-19 13:55:38 +0100

    refactor

--- test.js
+++ test.js
@@ -1,79 +1,79 @@
 var assert = require('assert')
 var index = require('./index')
 
-describe('bool', () => {
-	it('op', () => {
+describe('bool', function () {
+	it('op', function () {
 		var a = index.bool(false)
 		assert(a.op === 'bool')
 	})
-	it('false', () => {
+	it('false', function () {
 		var a = index.bool(false)
 		assert(a.val === false)
 	})
-	it('true', () => {
+	it('true', function () {
 		var a = index.bool(true)
 		assert(a.val === true)
 	})
-	it('length', () => {
+	it('length', function () {
 		var a = index.bool(false)
 		assert(a.length === 0)
 	})
 })
-describe('fun', () => {
-	it('name', () => {
+describe('fun', function () {
+	it('name', function () {
 		var a = index.fun('name')
 		assert(a.name === 'name')
 	})
-	it('length', () => {
+	it('length', function () {
 		var a = index.fun('name')
 		assert(a.length === 0)
 	})
 })
-describe('variable', () => {
-	it('name', () => {
+describe('variable', function () {
+	it('name', function () {
 		var a = index.variable('name')
 		assert(a.name === 'name')
 	})
-	it('length', () => {
+	it('length', function () {
 		var a = index.variable('name')
 		assert(a.length === 0)
 	})
 })
-describe('term', () => {
-	it('op', () => {
+describe('term', function () {
+	it('op', function () {
 		var x = index.fun()
 		var a = index.term('&', x)
 		assert(a.op === '&')
 	})
-	it('length', () => {
+	it('length', function () {
 		var x = index.fun()
 		var y = index.fun()
 		var z = index.fun()
 		var a = index.term('&', x, y, z)
 		assert(a.length === 3)
 	})
-	it('arg', () => {
+	it('arg', function () {
 		var x = index.fun()
 		var y = index.fun()
 		var z = index.fun()
 		var a = index.term('&', x, y, z)
 		assert(a[0] === x)
 	})
-	it('2nd arg', () => {
+	it('2nd arg', function () {
 		var x = index.fun()
 		var y = index.fun()
 		var z = index.fun()
 		var a = index.term('&', x, y, z)
 		assert(a[1] === y)
 	})
-	it('3rd arg', () => {
+	it('3rd arg', function () {
 		var x = index.fun()
 		var y = index.fun()
 		var z = index.fun()
 		var a = index.term('&', x, y, z)
 		assert(a[2] === z)
 	})
-	it('rest args', () => {
+	it('rest args', function () {
 		var x = index.fun()
 		var y = index.fun()
 		var z = index.fun()
@@ -82,8 +82,8 @@ describe('term', () => {
 		assert(a[2] === z)
 	})
 })
-describe('quant', () => {
-	it('op', () => {
+describe('quant', function () {
+	it('op', function () {
 		var vars = [
 			index.variable(),
 			index.variable(),
@@ -93,7 +93,7 @@ describe('quant', () => {
 		var a = index.quant('!', vars, arg)
 		assert(a.op === '!')
 	})
-	it('vars', () => {
+	it('vars', function () {
 		var vars = [
 			index.variable(),
 			index.variable(),
@@ -103,7 +103,7 @@ describe('quant', () => {
 		var a = index.quant('!', vars, arg)
 		assert.deepEqual(a.vars, vars)
 	})
-	it('length', () => {
+	it('length', function () {
 		var vars = [
 			index.variable(),
 			index.variable(),
@@ -113,7 +113,7 @@ describe('quant', () => {
 		var a = index.quant('!', vars, arg)
 		assert(a.length === 1)
 	})
-	it('arg', () => {
+	it('arg', function () {
 		var vars = [
 			index.variable(),
 			index.variable(),
@@ -124,8 +124,8 @@ describe('quant', () => {
 		assert(a[0] === arg)
 	})
 })
-describe('convert', () => {
-	it('constant', () => {
+describe('convert', function () {
+	it('constant', function () {
 		var a = {
 			args: [],
 			op: 'fun',
